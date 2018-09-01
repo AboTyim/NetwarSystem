16d6cb1bb153c51b932edafaba0c2f130e559699
Netwar System <nw@netwarsystem.com>
2018-08-31 18:24:36 -0700

    housecleaning re: ELK transition

--- settitle.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-import time, sys, setproctitle
-setproctitle.setproctitle(sys.argv[1])
-time.sleep(9999)
--- testfoo.py
+++
@@ -1,5 +0,0 @@
-import psutil
-import getpass
-for p in psutil.process_iter(attrs=['name', 'username']):
-        if (p.info['username'] == getpass.getuser()) and (p.info['name'] == "foo"):
-                print("running")
