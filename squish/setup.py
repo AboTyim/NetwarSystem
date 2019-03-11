434f6386c2b8ceb682fa0aff5208df16a335a313
Netwar System <nw@netwarsystem.com>
2019-03-11 12:57:42 -0700

    can I add squish here?

---
+++ squish/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='squish',
+      version='0.1',
+      description='Tools for Squishing Twitter Data',
+      url='https://github.com/NetwarSystem/squish',
+      author='NetwarSystem',
+      author_email='nope@netwarsystem.com',
+      license='MIT',
+      packages=['squish'],
+      zip_safe=False)
