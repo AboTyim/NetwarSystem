58feecef8e52769f821df33a0023320aa5423f94
Netwar System <nw@netwarsystem.com>
2019-08-20 17:06:03 -0700

    MONDO RESTRUCTURE, PART 2

---
+++ examples/README.md
@@ -0,0 +1,2 @@
+
+There are legitimate examples here as well as interesting code fragments from failed experiments that might turn up later as something useful.
---
+++ squish2/build/lib/squish2/perflog.py
@@ -0,0 +1,13 @@
+import datetime, platform, getpass
+def perflog(client, scname,mesg):
+	mydate = str(datetime.datetime.utcnow())
+	ebod = "{ \"index\" : { \"_index\" : \"perflog" + platform.node() + "\", \"_type\" : \"perfdata\"} }\n"
+	ebod = ebod + "{\"host\" : \"" + platform.node() + "\", \"shell\" : \"" + getpass.getuser() + "\", \"screen_name\" : \""
+	ebod = ebod + scname + "\", \"event\" : \"" + mesg + "\" , \"date\" : \"" + mydate + "\"}\n"
+	#print(ebod)
+	try:
+		client.bulk(body=ebod)
+	except(RuntimeError, TypeError, NameError):
+		print("can't get to client")
+		pass
+
